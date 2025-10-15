508ff1a8e55c864ad0c986d7b1a42df305d1a091
JordyRomeroa <jromeroa5@est.ups.edu.ec>
2025-10-14 21:03:51 -0500

    script.js

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
 
-  
-  const boton = document.getElementById("agregar-fila");
-  const tabla = document.getElementById("tabla-elementos").querySelector("tbody");
-
-  boton.addEventListener("click", () => {
-    const nuevaFila = document.createElement("tr");
-    nuevaFila.innerHTML = "<td>&lt;footer&gt;</td><td>Define el pie de página.</td>";
-    tabla.appendChild(nuevaFila);
-  });
-
-  
-  const titulo = document.getElementById("titulo");
-  titulo.addEventListener("mouseover", () => {
-    titulo.style.color = "#ff6600";
-  });
-  titulo.addEventListener("mouseout", () => {
-    titulo.style.color = "white";
-  });
-
-  alert("¡Bienvenido a mi práctica de Estándares Web!");
+document.getElementById("agregarFila").addEventListener("click", () => {
+  const tabla = document.querySelector("table");
+  const nuevaFila = tabla.insertRow();
+  nuevaFila.innerHTML = "<td>&lt;article&gt;</td><td>Sección independiente del contenido</td>";
 });
+
+
+const titulo = document.getElementById("titulo");
+titulo.addEventListener("mouseover", () => {
+  titulo.style.color = "#ff6600";
+});
+titulo.addEventListener("mouseout", () => {
+  titulo.style.color = "white";
+});
+
+
+alert("¡Bienvenido a mi página de Práctica Web!");
