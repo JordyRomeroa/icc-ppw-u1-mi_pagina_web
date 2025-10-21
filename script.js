247b2fecf677e2727f1c3b22b27243187d010cca
JordyRomeroa <jromeroa5@est.ups.edu.ec>
2025-10-20 22:04:01 -0500

    script.js

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
+// Mensaje al cargar la página
+window.onload = function() {
+    alert("¡Bienvenido a mi práctica de Estándares Web!");
+};
 
-document.getElementById("agregarFila").addEventListener("click", () => {
-  const tabla = document.querySelector("table");
-  const nuevaFila = tabla.insertRow();
-  nuevaFila.innerHTML = "<td>&lt;article&gt;</td><td>Sección independiente del contenido</td>";
-});
-
-
+// Cambiar color del título al pasar el mouse
 const titulo = document.getElementById("titulo");
-titulo.addEventListener("mouseover", () => {
-  titulo.style.color = "#ff6600";
+
+titulo.addEventListener("mouseover", function() {
+    titulo.style.color = "#28a745";
 });
-titulo.addEventListener("mouseout", () => {
-  titulo.style.color = "white";
+
+titulo.addEventListener("mouseout", function() {
+    titulo.style.color = "white";
+});
+
+// Agregar una nueva fila a la tabla
+const boton = document.getElementById("agregarFila");
+const tabla = document.getElementById("tablaElementos");
+
+boton.addEventListener("click", function() {
+    const nuevaFila = tabla.insertRow(-1);
+    const celda1 = nuevaFila.insertCell(0);
+    const celda2 = nuevaFila.insertCell(1);
+    celda1.innerHTML = "&lt;article&gt;";
+    celda2.innerHTML = "Sección de contenido independiente en la página";
 });
